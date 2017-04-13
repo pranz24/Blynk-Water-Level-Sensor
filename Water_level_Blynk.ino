2a05c4b50046ab02958509e7ae2e05d76b2d0cc1
Pranjal Tandon <pranjal.tandon@gmail.com>
2017-04-13 13:35:00 +0530

    Update Water_level_Blynk.ino

--- Water_level_Blynk.ino
+++ Water_level_Blynk.ino
@@ -3,13 +3,13 @@
 #include <BlynkSimpleEsp8266.h>
 
 // Authorization token from the Blynk App.
-char auth[] = "XXXXXXXXXXXXXXXX";
+char auth[] = "c039ef4a7aaa4eaa814d93e273c49f06";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "XXXXXXXX";
-char pass[] = "XXXXXXXX";
-char server[] = "XXXXXXXXXXX";
+char ssid[] = "G5_3513";
+char pass[] = "9650333228";
+char server[] = "192.168.48.30";
 
 
 const int sensorPin= A0; //sensor pin connected to analog pin A0
@@ -21,8 +21,9 @@ int bottom_level = 0;//Minimum water level
 
 void setup() {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass,server);
+  Blynk.begin(auth, ssid, pass, server);
   pinMode(sensorPin, INPUT);
+  Blynk.virtualWrite(V0, liquid_level);
   Blynk.virtualWrite(V1, liquid_percentage);//This wil show the percentage of water in the container in a virtual pin V1
   Blynk.run();
 }
@@ -33,10 +34,9 @@ void loop() {
   liquid_percentage = ((liquid_level-bottom_level)/top_level)*100;//Percentage of water in the container 
   Serial.println(liquid_level);//This will print the liquid level in the monitor 
   Serial.println(liquid_percentage);//This will print the percentage of liquid in the monitor
+  Blynk.virtualWrite(V0, liquid_level);
   Blynk.virtualWrite(V1, liquid_percentage);
-  delay(1000);
+  delay(100);
   Blynk.run();
 }
-
-
 
